2701ff65e0b5bb4af8302dc86237292856f9a4a7
CilooW <cecile.angays@yahoo.fr>
2016-06-19 00:32:46 +0200

    fini le plug in jquerygit push origin mastergit push origin mastergit push origin master!

--- application.js
+++ application.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
 	
-	$('#wherewhen').on('click','.btn',function(){
+	$('#wherewhen').on('click','button',function(){
+		var ok = $('<p>Ok! You shall pass and order <a href="#">here!</a></p>');
 		
-	$('.date').filter('.available').removeClass('btn-default').addClass('btn-success');
-	$('.date').filter('.complete').removeClass('btn-default').addClass('btn-danger');
+		if ($(this).filter('.available')){
+			$(this).filter('.available').text('');
+			$(this).filter('.available').append(ok).addClass('btn-success').removeClass('available');
+		}
+	
+
+		var no = $('<p>Awww! Sorry, there is no longer places for this date!</p>');
+		if ($(this).filter('.complete')){
+			$(this).filter('.complete').text('');
+			$(this).closest('.date').filter('.complete').append(no).addClass('btn-danger').removeClass('complete');
+		}
+		
 	});
-	
-	
 });
 
